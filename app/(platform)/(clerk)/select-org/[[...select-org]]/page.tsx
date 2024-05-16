c4e2e0410b8addd41be0096231f0784ff0d3343d
unknown <sarat.pint@gmail.com>
2024-05-16 09:05:49 +0700

    first commit

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,15 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+import React from 'react'
+
+const CreateOrganizationPage = () => {
+  return (
+    <OrganizationList
+     hidePersonal
+     afterSelectOrganizationUrl="/organization/:id"
+     afterCreateOrganizationUrl="/organization/:id"
+     />
+  )
+}
+
+export default CreateOrganizationPage
